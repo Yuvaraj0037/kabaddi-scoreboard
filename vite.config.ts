baefe72d616b776893a8f11e37a2da9508dad93d
Yuvaraj0037 <you@example.com>
2026-09-25 21:20:55 +0530

    Add PWA offline support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Kabaddi Scoreboard",
+        short_name: "Kabaddi Score",
+        description: "Offline digital Kabaddi scoreboard",
+
+        theme_color: "#020617",
+        background_color: "#020617",
+
+        display: "standalone",
+        orientation: "portrait",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
   ],
 });
